7263f80762c58c7ce1d0da450af95572d1cd29d6
storyexpression <116615215+storyexpression@users.noreply.github.com>
2022-11-07 15:51:10 +0900

    Delete assets/js directory

--- assets/js/header.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('mouseOver').addEventListener('mouseover',function() {
-  document.getElementById('header').classList.add('show');
-});
-
-document.getElementById('header').addEventListener('mouseleave',function() {
-  document.getElementById('header').classList.remove('show');
-});
