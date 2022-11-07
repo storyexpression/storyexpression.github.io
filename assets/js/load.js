d68472845c44ab773dba0ba2acf4d87239965625
storyexpression <116615215+storyexpression@users.noreply.github.com>
2022-11-07 21:19:02 +0900

    Add files via upload

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,3 +1,11 @@
-$(window).on('load',function(){
-  $('span').addClass('show');
-});
+let screenHeight = window.innerHeight;
+
+$('#load span').transit({
+  'opacity' : 1
+},1800);
+
+$('#load').transit({
+  'y' : -screenHeight,
+  'opacity' : 0,
+  'delay' : 2000
+},1800);
