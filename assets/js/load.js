b8053982bcd7b7f0be27140a616c215169b8f61e
storyexpression <116615215+storyexpression@users.noreply.github.com>
2022-11-10 22:43:05 +0900

    Add files via upload

--- assets/js/load.js
+++ assets/js/load.js
@@ -6,9 +6,17 @@ $('#load span').transit({
 },1800,pageUp);
 
 function pageUp(){
+  $('#load').addClass('loaded');
+};
+
+/*
+
+function pageUp(){
   $('#load').transit({
     'x' : -screenWidth,
     'y' : -screenHeight,
     'skewY' : '-90deg',
     'opacity' : 0
   },2800)};
+
+  */
