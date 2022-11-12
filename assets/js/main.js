3ac77eaaf4a2cf9c93bce421a0aa8b771d838444
storyexpression <116615215+storyexpression@users.noreply.github.com>
2022-11-12 19:44:16 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ if($(window).width() > 768) {
   $('header').on('mouseleave',function(){
     $('header').removeClass('show');
   });
-}
+};
 
 // toggle
 
@@ -16,4 +16,27 @@ $('#toggle').on('click',function(){
   $('#line2').toggleClass('line2');
   $('#line3').toggleClass('line3');
   $('#gNav').toggleClass('show');
+});
+
+// comicPages
+
+if($(window).width() > 768) {
+  $('#comicPage #mouseOver').on('mouseover',function() {
+    $('header').addClass('show');
+    $('footer').addClass('show');
+  });
+  $('#comicPage header').on('mouseleave',function(){
+    $('header').removeClass('show');
+    $('footer').removeClass('show');
+  });
+};
+
+$('.turnLeft'+','+'.turnRight').on('mouseover',function(){
+  $('.turnBtn').css('opacity','1');
+  $('.turnBtn a').css({'visibility':'visible','opacity':'1'});
+});
+
+$('.turnLeft'+','+'.turnRight').on('mouseleave',function(){
+  $('.turnBtn').css('opacity','0');
+  $('.turnBtn a').css({'visibility':'hidden','opacity':'0'});
 });
